397311b67a45bf71013c65b56db95e0507ed31e0
jxsontp <bladezjt@gmail.com>
2025-02-17 14:48:00 +0700

    Auth 1/3

---
+++ backend/api/views.py
@@ -0,0 +1,19 @@
+from django.contrib.auth.models import User
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
+@api_view(["POST"])
+def register(request):
+    username = request.data.get("username")
+    email = request.data.get("email")
+    password = request.data.get("password")
+
+    if not username or not email or not password:
+        return Response({"error": "All fields are required"}, status=status.HTTP_400_BAD_REQUEST)
+
+    if User.objects.filter(username=username).exists():
+        return Response({"error": "Username already taken"}, status=status.HTTP_400_BAD_REQUEST)
+
+    user = User.objects.create_user(username=username, email=email, password=password)
+    return Response({"message": "User registered successfully"}, status=status.HTTP_201_CREATED)
---
+++ backend/backend/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.urls import path, include
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    # path('api/shop/', include('shop.urls')),
+    path('auth/', include('dj_rest_auth.urls')),
+    path('auth/registration/', include('dj_rest_auth.registration.urls')),
+    path('auth/social/', include('allauth.socialaccount.urls')),
+
+
+    # API AUTH REGISTER 
+    path("api/", include("api.urls")),  # Pastikan ini ada
+
+]
